93ec38ff52604a785707697edc9e678d8084604c
Thiago Paiva <thiagohdaqw@gmail.com>
2022-12-15 01:43:20 -0300

    Adicionando conexao com servidor central

---
+++ distributed/central/server.py
@@ -0,0 +1,33 @@
+import threading
+import socket
+from queue import Queue
+
+from distributed.model.room import Room
+import distributed.central.consumer as consumer
+import distributed.central.producer as producer
+from distributed.util.bytes import int_to_bytes
+
+
+def init(host: str, port: int, room: Room, command_queue: Queue) -> tuple[Queue[Room], Queue]:
+    producer_queue = Queue()
+
+    conn = connect_central_server(host, port, room)
+
+    threading.Thread(target=consumer.consume,
+                     args=(conn, command_queue)).start()
+
+    threading.Thread(target=producer.produce,
+                     args=(conn, producer_queue)).start()
+
+    return producer_queue
+
+
+def connect_central_server(host, port, room):
+    conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    conn.connect((host, port))
+
+    conn.sendall(int_to_bytes(len(room.name)))
+    conn.sendall(room.name)
+
+    return conn
